3d2809a3f4ce6b35bb2f489d4335933b6dc79fba
Nathan <71136851+arandrollcode@users.noreply.github.com>
2020-10-03 14:08:38 -0500

    Fixed some stuff

--- MastermindAI.py
+++ MastermindAI.py
@@ -7,23 +7,14 @@
 ## Digits can have 6 possible values: (0,1,2,3,4,5)
 def generate_all_codes():
     all_codes = set()
-    code = [0.1] * 4
 
     ## TODO: Use some sort of loop to generate all possible codes
     
     for one in range(0,6):
-        code.pop(0)
-        code.insert(0, one)
         for two in range(0,6):
-            code.pop(1)
-            code.insert(1,two)
             for three in range(0,6):
-                code.pop(2)
-                code.insert(2,three)
                 for four in range(0,6):
-                    code.pop(3)
-                    code.insert(3,four)
-                    all_codes.add(tuple(code))
+                    all_codes.add((one,two,three,four))
             
     ## This line will check if you are returning the correct number of codes.
     assert(len(all_codes) == 1296)
@@ -96,14 +87,15 @@ class MastermindAI:
         
         self.all_codes = generate_all_codes()
         self.possible_codes = self.all_codes.copy()
-        self.previous_guesses   = list()
+        self.previous_guesses = list()
         self.previous_responses = list()
-
+        
     ## Based on possible_codes, calculate the best next guess
     ## TODO: Fill in the blanks and 
     def make_a_guess(self):
         ## If this is the first guess, guess 0011
         if len(self.previous_guesses) <= 0:
+            self.previous_guesses.append((0,0,1,1))
             return (0,0,1,1)
 
         ## If there is only 1 possible code, return it
@@ -112,7 +104,8 @@ class MastermindAI:
 
         ## TODO: Code Here
         if len(self.possible_codes) == 1:
-            return self.possible_codes
+            return self.possible_codes.pop()
+        
         ## Initialize some variables
         empty_response_count = dict()
         best_guess = (0,0,1,1)
@@ -159,7 +152,7 @@ class MastermindAI:
     ## Updates the set of possible codes by checking the response the code gives
     ## if it had been the actual code. If it does not match the response from the
     ## actual code, it cannot possible be the code, so it is removed.
-    def update(self, guess, response, code):
+    def update(self, guess, response):
         ## Add guess and response to previous_guesses and previous_responses
         self.previous_guesses.append(guess)
         self.previous_responses.append(response)
@@ -172,14 +165,14 @@ class MastermindAI:
         ## be a possible code. So we add it to the remove_set.
         ## https://www.w3schools.com/python/python_ref_set.asp
         ## TODO: Code Here
-        for thing in self.possible_codes:
-            if simulate_response(thing, code) != response:
-                remove_set.add(thing)
+        for code in self.possible_codes:
+            if simulate_response(guess, code) != response:
+                remove_set.add(code)
+
         ## Remove all codes in remove_set from self.possible_codes
         ## https://www.w3schools.com/python/python_ref_set.asp
 
         ## TODO: One Line of Code Here
-        print('remove set: ' + str(remove_set))
         self.possible_codes.difference_update(remove_set)
     ## Reset MastermindAI object to start a new game
     def reset(self):
@@ -197,26 +190,30 @@ if __name__ == "__main__":
     ai = MastermindAI()
 
     ## Print Introduction and Instructions
-    print('(´･･)ﾉHi! \nThis is an AI that will take your input code, and will determine it in under 5 guesses using an algorithm.')
+    print('━━(￣ー￣*|||━━\n(´･･)ﾉ is an AI that will take your input code, and will determine it in under 5 guesses using an algorithm.')
     
 
     #_`Wait for human to think of code and press Enter
     result = list()
-    code = input('Please enter a code: Ex:\'1234\'\n')
+    code = input('Enter a code with 4 digits and digits from 0-5 \nEx:\'1234\'\n')
     while not is_valid_guess(code):
-        print('Please enter a valid guess: Ex \'0123\'')
+        print('Enter a valid guess: Ex \'0123\'')
         code = input()
     for item in code:
         item = int(item)
         result.append(item)
     code = tuple(result)
     
-    for i in range(0,6):
+    while True:
         guessed = ai.make_a_guess()
         ## Print out a guess
-        print('Computer Guess: {0}\tActual Code: {1}'.format(guessed,code))
+        print('(´･･)ﾉ Guess: {0}\tActual Code: {1}\n'.format(guess_to_string(guessed),guess_to_string(code)))
+        if guessed == code:
+            break
         response = simulate_response(guessed, code)
-        ai.update(guessed,response,code)
+        ai.update(guessed,response)
+    print('(´･･)ﾉ figured out the code\nThanks for typing a code and pressing enter!')    
+
     ## Ask how many numbers are correct. Read user input.
     #input('How many numbers are correct?\nYour Code: {0}   Returned Code: {1}\nAnswer: '.format(code,guessed))
     ## Ask how many positions are correct. Read user input.
